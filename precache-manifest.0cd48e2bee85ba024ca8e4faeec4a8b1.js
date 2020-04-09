c09bbeafb908a7ebc4cc2643ed8e5cec2434d12c
s_harada <satoshi.harada@trustia.jp>
2020-04-09 18:02:39 +0900

    Updates

---
+++ precache-manifest.0cd48e2bee85ba024ca8e4faeec4a8b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155d68b74677016c9b0329b770647a84",
+    "url": "/furnitures-admin/index.html"
+  },
+  {
+    "revision": "25df4d8b350c83ff95a6",
+    "url": "/furnitures-admin/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "c4cde52c66bd5a2a4d2b",
+    "url": "/furnitures-admin/static/js/2.17302e95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/furnitures-admin/static/js/2.17302e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25df4d8b350c83ff95a6",
+    "url": "/furnitures-admin/static/js/main.f368d8a7.chunk.js"
+  },
+  {
+    "revision": "0aa8d4b7fbf4ebb7b91c",
+    "url": "/furnitures-admin/static/js/runtime-main.bdff9a5c.js"
+  }
+]);
